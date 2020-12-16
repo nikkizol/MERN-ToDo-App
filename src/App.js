4b3137c5b8b435f35c508cd55320b9c712c2f98c
MZ <nikita.zholkovskyi@gmail.com>
2020-12-16 11:06:39 +0100

    added three empty components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <div className="container">
         <h2>MERN-Stack Todo App</h2>
       </div>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
     </Router>
   );
 }
